3b20e5040a3d6c384e4c81512ae565b1d68675f5
Ryan Zmuda <ryanzmuda@gmail.com>
2021-02-09 23:55:37 -0500

    :hammer: doin stuff

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 600,
-    minWidth: 1130,
-    minHeight: 626,
-    maxHeight: 626,
     frame: false,
     webPreferences: {
       nodeIntegration: true,
--- scripts/menuHandler.js
+++ scripts/menuHandler.js
@@ -8,3 +8,17 @@ function closeWindow(){
 function minWindow(){
     win.minimize();
 }
+
+function dropDownBox() {
+    document.getElementById("gameDropdown").classList.toggle("show");
+    document.getElementById("dropDown").classList.toggle("upwards");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.gameSelect')) {
+        var myDropdown = document.getElementById("dropContent");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
